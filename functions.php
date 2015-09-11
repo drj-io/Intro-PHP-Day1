71991e35bdf5c96c35442121bd5f133a8cb0b8bf
David R. Jones <david.jones@madwiremedia.com>
2015-09-10 20:37:53 -0600

    using php inside

---
+++ functions.php
@@ -0,0 +1,68 @@
+<?php
+$companies[] = array(
+  "name" => "Madwire",
+  "address" => "550 West Eisenhower Blvd",
+  "city" => "Loveland",
+  "state" => "CO",
+  "zip" => "80537",
+  "employees" => array('david','joe','jb','jerry')
+    );
+$companies[] = array(
+  "name" => "IBM",
+  "address" => "123 IBM Drive",
+  "city" => "Boulder",
+  "state" => "CO",
+  "zip" => "80301",
+  "employees" => array('david','joe','jb','jerry')
+);
+$companies[] = array(
+  "name" => "HP",
+  "address" => "123 HP Drive",
+  "city" => "Fort Collins",
+  "state" => "CO",
+  "zip" => "80524",
+  "employees" => array('david','joe','jb','jerry')
+);
+$companies[] = array(
+  "name" => "Digital Globe",
+  "address" => "123 DG Street",
+  "city" => "Longmont",
+  "state" => "CO",
+  "zip" => "80501",
+  "employees" => array('david','joe','jb','jerry')
+);
+
+ ?><!doctype html>
+<html>
+<head>
+
+</head>
+<body>
+  <h1>Hi</h1>
+
+  <table>
+    <?php foreach($companies as $company) { ?>
+      <tr>
+        <td><?php print $company['name']; ?></td>
+        <td><?php print $company['address']; ?></td>
+        <td><?php print $company['city']; ?></td>
+      </tr>
+      <?php } ?>
+
+  </table>
+
+
+<style>
+  table { width: 100%; }
+  td { border: 1px solid black}
+
+</style>
+
+</body>
+
+
+<?php
+
+
+
+?>
--- phpscript.php
+++ phpscript.php
@@ -2,8 +2,11 @@
 
 
 echo "hello world \\n\n\n...\n";
+
+sleep(3);
+
 $name = "david";
 
-echo "hello  \$name \n"; 
+echo "hello  \$name \n";
 
 ?>
